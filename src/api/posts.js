deff4b20e1fae16ea4efb0c0fadf66309c64306a
shinhonggyu <zowoz0@naver.com>
2022-03-06 05:55:09 +0900

    middleware, thunk, json-server

--- src/api/posts.js
+++ src/api/posts.js
@@ -2,12 +2,12 @@ import axios from "axios";
 
 // 포스트 목록을 가져오는 비동기 함수
 export const getPosts = async () => {
-  const { data } = await axios.get("http://localhost:4000/posts");
+  const { data } = await axios.get("/posts");
   return data;
 };
 
 // ID로 포스트를 조회하는 비동기 함수
 export const getPostById = async (id) => {
-  const { data } = await axios.get(`http://localhost:4000/posts/${id}`);
+  const { data } = await axios.get(`/posts/${id}`);
   return data;
 };
